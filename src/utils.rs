72a3640ace9d06446ba997e0252d26fae1faaefc
DontPanicO <dontpanic.call0@gmail.com>
2026-02-15 20:44:20 +0100

    add `write_all` helper

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::{io, os::fd::BorrowedFd};
+
 pub trait RetCode: Copy {
     fn is_error(self) -> bool;
 }
@@ -39,4 +41,13 @@ pub fn is_crash_signal(sig: i32) -> bool {
             | libc::SIGILL
             | libc::SIGBUS
     )
+}
+
+#[inline(always)]
+pub fn write_all(fd: BorrowedFd<'_>, mut buf: &[u8]) -> io::Result<()> {
+    while !buf.is_empty() {
+        let n = rustix::io::write(fd, buf)?;
+        buf = &buf[n..];
+    }
+    Ok(())
 }
