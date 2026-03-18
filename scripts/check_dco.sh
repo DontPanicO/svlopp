2458431b64c6f5d5ca317ed4758576b78e344009
DontPanicO <dontpanic.call0@gmail.com>
2026-03-18 17:07:01 +0100

    update DCO check script to ignore github actions "merge" commits
    Signed-off-by: DontPanicO <dontpanic.call0@gmail.com>

--- scripts/check_dco.sh
+++ scripts/check_dco.sh
@@ -19,6 +19,14 @@ for c in $commits; do
     continue
   fi
 
+  # It appears that github actions "merge" commits are not really merge
+  # commits and they have 1 parent only. I can't find a better solution
+  # than match the messagge pattern
+  if git log -1 --pretty=%B "$c" | grep -q "^Merge .* into .*"; then
+    echo "Skipping synthetic merge commit $c"
+    continue
+  fi
+
   if ! git log -1 --pretty=%B "$c" | grep -qi "^Signed-off-by:"; then
     echo "Commit $c is missing Signed-off-by"
     git log -1 --pretty=full "$c"
