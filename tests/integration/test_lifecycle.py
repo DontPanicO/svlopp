ac254b083cf2502a4b5e60b0db579c79bc9a3ce1
DontPanicO <dontpanic.call0@gmail.com>
2026-03-23 09:44:05 +0100

    move tests from `test_service_termination.py` to `test_lifecycle.py`
    Signed-off-by: DontPanicO <dontpanic.call0@gmail.com>

--- tests/integration/test_lifecycle.py
+++ tests/integration/test_lifecycle.py
@@ -12,6 +12,7 @@ from constants import (
     STATE_RUNNING,
     STATE_STOPPED,
     REASON_EXITED,
+    REASON_SIGNALED,
     CONFIG_FILE_NAME,
 )
 
@@ -167,6 +168,152 @@ working_directory = "/does/not/exist"
 
     assert line.state == STATE_STOPPED
     assert line.pid_or_reason == f"{REASON_EXITED}(111)"
+
+
+def test_service_signaled(tmp_path, run_dir, svlopp_proc):
+    config_path = tmp_path / CONFIG_FILE_NAME
+    config_path.write_text(
+        """
+[services.test]
+command = "sleep"
+args = ["10"]
+"""
+    )
+
+    _ = svlopp_proc(config_path)
+
+    def is_running():
+        try:
+            status = read_status(run_dir)
+            return status.is_running("test")
+        except (FileNotFoundError, KeyError):
+            return False
+
+    wait_until(is_running, timeout=1.0)
+
+    status = read_status(run_dir)
+    line = status.get("test")
+    pid = int(line.pid_or_reason)
+
+    os.kill(pid, signal.SIGKILL)
+
+    def is_stopped():
+        try:
+            status = read_status(run_dir)
+            return status.is_stopped("test")
+        except (FileNotFoundError, KeyError):
+            return False
+
+    wait_until(is_stopped, timeout=3.0)
+
+    status = read_status(run_dir)
+    line = status.get("test")
+
+    assert line.state == STATE_STOPPED
+    assert line.pid_or_reason == f"{REASON_SIGNALED}(9)"
+    assert not Path(f"/proc/{pid}").exists()
+
+
+# crashed and killed are currently both represented as "signaled(...)"
+# in the status file. This test will become stricter if that changes.
+def test_service_crashed(tmp_path, run_dir, svlopp_proc):
+    config_path = tmp_path / CONFIG_FILE_NAME
+    config_path.write_text(
+        """
+[services.test]
+command = "sleep"
+args = ["10"]
+"""
+    )
+
+    _ = svlopp_proc(config_path)
+
+    def is_running():
+        try:
+            status = read_status(run_dir)
+            return status.is_running("test")
+        except (FileNotFoundError, KeyError):
+            return False
+
+    wait_until(is_running, timeout=1.0)
+
+    status = read_status(run_dir)
+    line = status.get("test")
+
+    pid = int(line.pid_or_reason)
+
+    os.kill(pid, signal.SIGSEGV)
+
+    def is_stopped():
+        try:
+            status = read_status(run_dir)
+            return status.is_stopped("test")
+        except (FileNotFoundError, KeyError):
+            return False
+
+    wait_until(is_stopped, timeout=3.0)
+
+    status = read_status(run_dir)
+    line = status.get("test")
+
+    assert line.state == STATE_STOPPED
+    assert line.pid_or_reason == f"{REASON_SIGNALED}(11)"
+    assert not Path(f"/proc/{pid}").exists()
+
+
+def test_service_error(tmp_path, run_dir, svlopp_proc):
+    config_path = tmp_path / CONFIG_FILE_NAME
+    config_path.write_text(
+        """
+[services.test]
+command = "/bin/sh"
+args = ["-c", "exit 1"]
+"""
+    )
+
+    _ = svlopp_proc(config_path)
+
+    def is_stopped():
+        try:
+            status = read_status(run_dir)
+            return status.is_stopped("test")
+        except (FileNotFoundError, KeyError):
+            return False
+
+    wait_until(is_stopped, timeout=3.0)
+
+    status = read_status(run_dir)
+    line = status.get("test")
+
+    assert line.state == STATE_STOPPED
+    assert line.pid_or_reason == f"{REASON_EXITED}(1)"
+
+
+def test_service_success(tmp_path, run_dir, svlopp_proc):
+    config_path = tmp_path / CONFIG_FILE_NAME
+    config_path.write_text(
+        """
+[services.test]
+command = "/bin/true"
+"""
+    )
+
+    _ = svlopp_proc(config_path)
+
+    def is_stopped():
+        try:
+            status = read_status(run_dir)
+            return status.is_stopped("test")
+        except (FileNotFoundError, KeyError):
+            return False
+
+    wait_until(is_stopped, timeout=3.0)
+
+    status = read_status(run_dir)
+    line = status.get("test")
+
+    assert line.state == STATE_STOPPED
+    assert line.pid_or_reason == f"{REASON_EXITED}(0)"
 
 
 def test_graceful_shutdown(tmp_path, run_dir, svlopp_proc):
