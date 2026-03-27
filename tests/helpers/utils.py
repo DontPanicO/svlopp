f17118d3c1324d058e939e3cff792f7c5327cbb0
DontPanicO <dontpanic.call0@gmail.com>
2026-03-27 12:10:32 +0100

    add `pid_exists` helper for tests
    Signed-off-by: DontPanicO <dontpanic.call0@gmail.com>

--- tests/helpers/utils.py
+++ tests/helpers/utils.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+from pathlib import Path
 import time
 
 
@@ -22,3 +23,7 @@ def is_zombie(pid: int) -> bool:
         return state == "Z"
     except FileNotFoundError:
         return False
+
+
+def pid_exists(pid: int | str) -> bool:
+    return Path(f"/proc/{pid}").exists()
